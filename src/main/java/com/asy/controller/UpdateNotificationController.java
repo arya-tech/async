416bc838340980578af1d6399906e21ccd95da08
Arya <aryadewan123@gmail.com>
2021-06-16 15:24:15 +0530

    added UpdateNotificationController component

---
+++ src/main/java/com/asy/controller/UpdateNotificationController.java
@@ -0,0 +1,10 @@
+package com.asy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/update")
+public class UpdateNotificationController {
+
+}

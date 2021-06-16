67fc5bc61a9be36c22193dde31f02b9cbc09bb2a
Arya <aryadewan123@gmail.com>
2021-06-16 15:27:24 +0530

    added DeleteNotificationController

---
+++ src/main/java/com/asy/controller/DeleteNotificationController.java
@@ -0,0 +1,10 @@
+package com.asy.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/delete")
+public class DeleteNotificationController {
+
+}
